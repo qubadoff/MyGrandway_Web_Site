d02803d7aa434967d09ebedec3e92bd554192b41
ToghrulAliyev <togrulali4@gmail.com>
2024-03-08 19:11:11 +0400

    fix toggle

--- main.js
+++ main.js
@@ -1,14 +1,28 @@
 document.addEventListener('DOMContentLoaded', function () {
-    var elements = document.querySelectorAll('.process--number-black');
-    elements.forEach(function (element) {
-      element.addEventListener('click', function () {
-        this.classList.toggle('process--number');
-        this.classList.toggle('process--number-black');
-        var imgTag = this.querySelector('img');
-        imgTag.classList.toggle("rotateImg")
-        imgTag.classList.toggle("rotateBack")
-         var paragraph = this.parentElement.querySelector('.open-answer');
-         paragraph.classList.toggle('question--card-questions-open');
+  var elements = document.querySelectorAll('.process--number-black');
+  elements.forEach(function (element) {
+    element.addEventListener('click', function () {
+      // Close all elements except the one that was clicked
+      elements.forEach(function (otherElement) {
+        if (otherElement !== element) {
+          otherElement.classList.remove('process--number');
+          otherElement.classList.add('process--number-black');
+          var otherImgTag = otherElement.querySelector('img');
+          otherImgTag.classList.remove("rotateImg");
+          otherImgTag.classList.add("rotateBack");
+          var otherParagraph = otherElement.parentElement.querySelector('.open-answer');
+          otherParagraph.classList.remove('question--card-questions-open');
+        }
       });
+
+      // Toggle the class of the clicked element
+      this.classList.toggle('process--number');
+      this.classList.toggle('process--number-black');
+      var imgTag = this.querySelector('img');
+      imgTag.classList.toggle("rotateImg");
+      imgTag.classList.toggle("rotateBack");
+      var paragraph = this.parentElement.querySelector('.open-answer');
+      paragraph.classList.toggle('question--card-questions-open');
     });
   });
+});
